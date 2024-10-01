02b44d06ac193a24103cdc13f85f98f0f105d0e8
Rodrigo Veras <97261906+TheVeras@users.noreply.github.com>
2024-10-01 15:35:31 -0300

    Update exercicios_senac.py

--- Python Senac/exercicios_senac.py
+++ Python Senac/exercicios_senac.py
@@ -11,7 +11,6 @@ print(f"A quantidade de números pares é {cont}")
 # 2 - Crie uma lista com seis números inteiros. 
 # Em seguida, use um laço para  somar todos os números 
 # da lista que são maiores que 5 e impirma o resultado 
-numeros = [14,28,3,48,5,6]
 soma = 0
 cont = 0
 for num in numeros:
@@ -23,7 +22,6 @@ print(f"Existem {cont} números maiores que 5 e a soma dele é: {soma}")
 # 3 - Crie uma lista com seis números inteiros. 
 # Em seguida, use um laço para  contar quantos números ímpares 
 # existem na lista e imprima o resultado.
-numeros = [14,28,3,48,5,6]
 cont = 0
 for num in numeros:
     if num % 2 != 0:
